67b9e5fe18aa0aac164025cf35b4720287bbf6f1
Pierre LALET <pierre.lalet@cea.fr>
2014-10-19 20:44:27 +0200

    Added CVS export support (scancli)

--- ivre/utils.py
+++ ivre/utils.py
@@ -285,6 +285,48 @@ def diff(doc1, doc2):
                 # print kkey
     return res
 
+def fields2csv_head(fields, prefix=''):
+    line = []
+    for field, subfields in fields.iteritems():
+        if subfields is True or callable(subfields):
+            line.append(prefix + field)
+        elif isinstance(subfields, dict):
+            line += fields2csv_head(subfields,
+                                    prefix=prefix + field + '.')
+    return line
+
+def doc2csv(doc, fields):
+    lines = [[]]
+    for field, subfields in fields.iteritems():
+        if subfields is True:
+            value = doc.get(field)
+            if type(value) is list:
+                lines = [line + [valelt]
+                         for line in lines for valelt in value]
+            else:
+                lines = [line + [value] for line in lines]
+        elif callable(subfields):
+            value = doc.get(field)
+            if type(value) is list:
+                lines = [line + [subfields(valelt)]
+                         for line in lines for valelt in value]
+            else:
+                lines = [line + [subfields(value)] for line in lines]
+        elif isinstance(subfields, dict):
+            subdoc = doc.get(field)
+            if type(subdoc) is list:
+                lines = [line + newline
+                         for line in lines
+                         for subdocelt in subdoc
+                         for newline in doc2csv(subdocelt,
+                                                subfields)]
+            elif subdoc is not None:
+                lines = [line + newline
+                         for line in lines
+                         for newline in doc2csv(subdoc,
+                                                subfields)]
+    return lines
+
 
 class FakeArgparserParent(object):
     """This is a stub to implement a parent-like behavior when
