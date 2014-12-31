4a08d134e491b57d76ce018a003a4a0821d859fe
Pierre LALET <pierre.lalet@cea.fr>
2014-12-31 12:55:49 +0100

    Remove no longer used function to create pie graphs

--- web/static/ivre.js
+++ web/static/ivre.js
@@ -118,46 +118,6 @@ function heatmapColour(value) {
     return "hsl(" + h + ", 100%, 50%)";
 }
 
-function build_chart_pie(chart, field, data) {
-    var w = 500,
-    h = 500,
-    r = 200,
-    color = d3.scale.category10();
-    
-    var vis = d3.select("#"+chart)
-	.append("svg:svg")
-	.data([data])
-	.attr("width", w)
-	.attr("height", h)
-	.append("svg:g")
-	.attr("transform", "translate(" + w/2 + "," + h/2 + ")");
-    
-    var arc = d3.svg.arc()
-	.outerRadius(r);
-    
-    var pie = d3.layout.pie()
-	.value(function(d) { return d.value; });
-    
-    var arcs = vis.selectAll("g.slice")
-	.data(pie)
-	.enter()
-	.append("svg:g")
-	.attr("class", "slice");
-    
-    arcs.append("svg:path")
-	.attr("fill", function(d, i) { return color(i); })
-	.attr("d", arc);
-    
-    arcs.append("svg:text")
-	.attr("transform", function(d) {
-	    d.innerRadius = 0;
-	    d.outerRadius = r;
-	    return "translate(" + arc.centroid(d) + ")";
-	})
-	.attr("text-anchor", "middle")
-	.text(function(d) { return d.label; });
-}
-
 function build_chart_plane(chart, ips) {
     var w = 500,
     h = 450,
