7ad2bd8d25f8ac74e8b55a5f18d9886668ae9a94
Pierre LALET <pierre.lalet@cea.fr>
2015-12-15 15:44:42 +0100

    WebUI: change object pattern for Filter (thanks @commial)

--- web/static/ivre/filters.js
+++ web/static/ivre/filters.js
@@ -1,69 +1,76 @@
 var FILTERS = {}, FILTER;
 
-function _compare_params(store, other, count) {
-    /* Returns true iff store "contains" other. "limit", "skip" and
-     * "sortby" are omitted when count is true. */
-    for(var key in store) {
-	if((count && (key == 'limit' || key == 'skip' || key == 'sortby')) ||
-	   key == 'display') {
-	    continue;
+var Filter = (function() {
+    function Filter(name) {
+	this.parameters = [];
+	this.parametersprotected = [];
+	this.parametersobj = {};
+	this.parametersobjunalias = {};
+	this.count = undefined;
+	// the initial prev_query has to be an object and to be
+	// different than any valid query
+	this.prev_query = {"thiswillneverexist": []};
+	if(name) {
+	    FILTERS[name] = this;
+	    this.idprefix = name + "-";
 	}
-	if(other[key] === undefined) {
-	    return false;
+	else {
+	    FILTER = this;
+	    this.idprefix = "";
 	}
-	next_index:
-	for(var index in store[key]) {
-	    for(var other_index in other[key]) {
-		if(store[key][index][0] === other[key][other_index][0] &&
-		   store[key][index][1] === other[key][other_index][1]) {
-		    continue next_index;
+    }
+
+    function _compare_params(store, other, count) {
+	/* Returns true iff store "contains" other. "limit", "skip"
+	 * and "sortby" are omitted when count is true. */
+	for(var key in store) {
+	    if((count && (key == 'limit' || key == 'skip'
+			  || key == 'sortby')) ||
+	       key == 'display') {
+		continue;
+	    }
+	    if(other[key] === undefined) {
+		return false;
+	    }
+	    next_index:
+	    for(var index in store[key]) {
+		for(var other_index in other[key]) {
+		    if(store[key][index][0] === other[key][other_index][0] &&
+		       store[key][index][1] === other[key][other_index][1]) {
+			continue next_index;
+		    }
 		}
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    $.extend(Filter.prototype, {
+	end_new_query: function() {
+	    this.prev_query = {};
+	    for(var key in this.parametersobjunalias) {
+		this.prev_query[key] = this.parametersobjunalias[key];
+	    }
+	},
+
+	need_update: function() {
+	    return ! this.compare_params(false);
+	},
+
+	need_count: function() {
+	    return ! this.compare_params(true);
+	},
+
+	compare_params: function(count) {
+	    if(_compare_params(this.parametersobjunalias,
+			       this.prev_query, count)) {
+		return _compare_params(this.prev_query,
+				       this.parametersobjunalias, count);
 	    }
 	    return false;
 	}
-    }
-    return true;
-}
+    });
 
-var Filter = function(name) {
-    this.parameters = [];
-    this.parametersprotected = [];
-    this.parametersobj = {};
-    this.parametersobjunalias = {};
-    this.count = undefined;
-    // the initial prev_query has to be an object and to be different
-    // than any valid query
-    this.prev_query = {"thiswillneverexist": []};
-    if(name) {
-	FILTERS[name] = this;
-	this.idprefix = name + "-";
-    }
-    else {
-	FILTER = this;
-	this.idprefix = "";
-    }
-};
-
-Filter.prototype.end_new_query = function() {
-    this.prev_query = {};
-    for(var key in this.parametersobjunalias) {
-	this.prev_query[key] = this.parametersobjunalias[key];
-    }
-};
-
-Filter.prototype.need_update = function() {
-    return ! this.compare_params(false);
-};
-
-Filter.prototype.need_count = function() {
-    return ! this.compare_params(true);
-};
-
-Filter.prototype.compare_params = function(count) {
-    if(_compare_params(this.parametersobjunalias,
-		       this.prev_query, count)) {
-	return _compare_params(this.prev_query,
-			       this.parametersobjunalias, count);
-    }
-    return false;
-};
+    return Filter;
+})();
