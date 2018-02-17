6e114c6801c24e45501f27b3f9fbbf17c75d3183
fccagou <fccagou@gmail.com>
2018-02-17 17:47:40 +0100

    refact(parser/iptables): update code according to pep8

--- ivre/parser/iptables.py
+++ ivre/parser/iptables.py
@@ -19,22 +19,25 @@
 # Maintainers:
 #   - 2018 Francois CHENAIS <francois.chenais@cea.fr>
 #
-
+#
+# Feb  4 05:30:11 pi01 kernel: [3240403.495065]
+# IPTABLES/UDP/IN=enxb827eb8f8a4f
+# OUT=
+# MAC=ff:ff:ff:ff:ff:ff:18:62:2c:7e:45:d0:08:00:45:00:00:ec:00:00:40:00:...
+# SRC=192.168.0.254
+# DST=192.168.0.255
+# LEN=236 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=138 DPT=138 LEN=216
+#
+# TODO:
+#  - the log does not contain the Year so it's necessary to set it using
+#    parameter or changing input with new field 'YEAR' or using current year.
+#  - The log contains mac addr from source and destination, is this inifo can
+#    be added to neo db ?
+#
 """Support for Iptables log from syslog files
-
-
-Feb  4 05:30:11 pi01 kernel: [3240403.495065] IPTABLES/UDP/IN=enxb827eb8f8a4f OUT= MAC=ff:ff:ff:ff:ff:ff:18:62:2c:7e:45:d0:08:00:45:00:00:ec:00:00:40:00:40:11:df:e3 SRC=192.168.0.254 DST=192.168.0.255 LEN=236 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=138 DPT=138 LEN=216
-
-TODO:
- - the log does not contain the Year so it's necessary to set it using parameter
-   or changing input with new field 'YEAR' or using current year.
- - The log contains mac addr from source and destination, is this inifo can be
-   added to neo db ?
-
 """
 
 import datetime
-from builtins import zip
 from ivre.parser import Parser
 
 
@@ -44,20 +47,24 @@ class Iptables(Parser):
     def __init__(self, fname, pcap_filter=None):
         super(Iptables, self).__init__(fname)
 
-
     def parse_line(self, line):
 
-        field_idx=line.find(b'IN=')
-        if field_idx<0:
+        field_idx = line.find(b'IN=')
+        if field_idx < 0:
             # It's not an iptables log
             return next(self)
 
         # Converts the syslog iptables log into hash
-        fields = dict((key.lower(), value)
-                for key, value in (val.split(b'=', 1) if b'=' in val else (val, b'')
-                    for val in line[field_idx:].rstrip(b'\r\n').split()))
+        fields = dict(
+               (key.lower(), value)
+               for key, value in (val.split(b'=', 1)
+               if b'=' in val else (val, b'')
+               for val in line[field_idx:].rstrip(b'\r\n').split())
+           )
+
         try:
-            fields[b'start_time'] = datetime.datetime.strptime(line[:15].decode(), "%b %d %H:%M:%S")
+            fields[b'start_time'] = datetime.datetime.strptime(
+                                        line[:15].decode(), "%b %d %H:%M:%S")
         except ValueError:
             # Bad Date format
             return next(self)
@@ -69,8 +76,10 @@ class Iptables(Parser):
             fields[b'sport'] = int(fields[b'spt'])
             fields[b'dport'] = int(fields[b'dpt'])
 
-        # This data is mandatory but undefined in iptables logs, so make my/a choice.
-        fields[b'cspkts'] = fields[b'scpkts'] = fields[b'scbytes'] = fields[b'csbytes'] = 0
+        # This data is mandatory but undefined in iptables logs, so make
+        # a choice.
+        fields[b'cspkts'] = fields[b'scpkts'] = 0
+        fields[b'scbytes'] = fields[b'csbytes'] = 0
         fields[b'end_time'] = fields[b'start_time']
 
         return fields
