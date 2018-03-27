13a036aaf7698e0e7581a9f13f2462eb07b105b2
Pierre LALET <pierre.lalet@cea.fr>
2018-03-27 17:39:05 +0200

    Web w/ Postgres: display query in debug mode

--- ivre/web/app.py
+++ ivre/web/app.py
@@ -361,7 +361,7 @@ def get_nmap():
         yield webutils.js_del_alert("param-unused")
 
     if config.DEBUG:
-        msg1 = "filter: %r" % flt_params.flt
+        msg1 = "filter: %s" % db.nmap.flt2str(flt_params.flt)
         msg2 = "user: %r" % webutils.get_user()
         utils.LOGGER.debug(msg1)
         utils.LOGGER.debug(msg2)
