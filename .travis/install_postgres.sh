48d288de1a8b2cd5a745a59cfbdf2c79bd0ec05c
Pierre LALET <pierre.lalet@cea.fr>
2018-11-07 13:38:00 +0100

    Add missing (c) notices

--- .travis/install_postgres.sh
+++ .travis/install_postgres.sh
@@ -1,5 +1,21 @@
 #! /bin/sh
 
+# This file is part of IVRE.
+# Copyright 2011 - 2018 Pierre LALET <pierre.lalet@cea.fr>
+#
+# IVRE is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IVRE is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with IVRE. If not, see <http://www.gnu.org/licenses/>.
+
 wget -O pgsql.tgz "http://get.enterprisedb.com/postgresql/postgresql-${POSTGRES_VERSION}-1-linux-x64-binaries.tar.gz"
 tar zxf pgsql.tgz
 rm pgsql.tgz
--- .travis/install_sqlite.sh
+++ .travis/install_sqlite.sh
@@ -1,6 +1,22 @@
 #! /bin/sh
 
+# This file is part of IVRE.
+# Copyright 2011 - 2018 Pierre LALET <pierre.lalet@cea.fr>
+#
+# IVRE is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IVRE is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with IVRE. If not, see <http://www.gnu.org/licenses/>.
+
 mkdir -p /tmp/ivredb
 sudo mount -t tmpfs tmpfs /tmp/ivredb -o users,uid=travis,gid=travis,mode=0777
 
-echo 'DB="sqlite:////tmp/ivredb/ivre.db"' >> ~/.ivre.conf
+echo 'DB = "sqlite:////tmp/ivredb/ivre.db"' >> ~/.ivre.conf
