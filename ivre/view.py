9d0234610828183b13c121d6e48a19ccb78f967d
Pierre LALET <pierre.lalet@cea.fr>
2019-02-15 10:36:22 +0100

    View from passive: a DNS_ANSWER is not enough to mark a host as up

--- ivre/view.py
+++ ivre/view.py
@@ -209,10 +209,12 @@ def passive_record_to_view(rec):
         return
     outrec = {
         'addr': rec["addr"],
-        'state': "up",
         'state_reason': 'passive',
         'schema_version': SCHEMA_VERSION,
     }
+    # a DNS_ANSWER record is not enough to mark a host as up
+    if rec['recontype'] != 'DNS_ANSWER':
+        outrec['state'] = 'up'
     sensor = rec.get('sensor')
     if sensor:
         outrec['source'] = sensor
