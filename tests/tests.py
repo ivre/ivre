1c1493cc4bfcd992b9fba1beb38eaa33e25f9561
Pierre LALET <pierre.lalet@cea.fr>
2019-09-13 11:08:38 +0200

    DB/Elastic/View: (partial) support for .topvalues("ja3-client([:.]*)")

--- tests/tests.py
+++ tests/tests.py
@@ -3726,13 +3726,6 @@ purposes to feed Elasticsearch view.
                                   "httphdr:content-type")
         self.check_view_top_value("view_http_top_ua", "useragent")
         self.check_view_top_value("view_http_top_ua_curl", "useragent:/^curl/")
-
-        if DATABASE == "elastic":
-            # Support for Elasticsearch is experimental and lacks a
-            # lot of functionalities. The next tests will fail for
-            # lack of filters & topvalues.
-            return
-
         self.check_view_top_value("view_ssl_top_ja3cli_md5", "ja3-client")
         self.check_view_top_value("view_ssl_top_ja3cli_md5", "ja3-client.md5")
         self.check_view_top_value("view_ssl_top_ja3cli_sha1",
@@ -3740,6 +3733,15 @@ purposes to feed Elasticsearch view.
         self.check_view_top_value("view_ssl_top_ja3cli_sha256",
                                   "ja3-client.sha256")
         self.check_view_top_value("view_ssl_top_ja3cli_raw", "ja3-client.raw")
+        self.check_view_top_value("view_ssl_top_ja3cli_raw_771",
+                                  "ja3-client.raw:/^771/")
+
+        if DATABASE == "elastic":
+            # Support for Elasticsearch is experimental and lacks a
+            # lot of functionalities. The next tests will fail for
+            # lack of filters & topvalues.
+            return
+
         self.check_view_top_value("view_ssl_top_ja3cli_md5_771",
                                   "ja3-client:/^771/")
         self.check_view_top_value("view_ssl_top_ja3cli_md5_771",
@@ -3748,8 +3750,6 @@ purposes to feed Elasticsearch view.
                                   "ja3-client.sha1:/^771/")
         self.check_view_top_value("view_ssl_top_ja3cli_sha256_771",
                                   "ja3-client.sha256:/^771/")
-        self.check_view_top_value("view_ssl_top_ja3cli_raw_771",
-                                  "ja3-client.raw:/^771/")
 
         self.check_view_top_value("view_ssl_top_ja3srv_md5", "ja3-server")
         self.check_view_top_value("view_ssl_top_ja3srv_md5", "ja3-server.md5")
