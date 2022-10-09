232c0a425069ab1bbe279d747f905a725c3d3aba
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 21:47:53 +0000

    Adding tarfile member sanitization to extractall()

--- tests/tests.py
+++ tests/tests.py
@@ -5001,7 +5001,26 @@ class IvreTests(unittest.TestCase):
         self.assertTrue(os.path.exists(up_files[0]))
         with tarfile.open(data_files[0]) as data_archive:
             data_archive = tarfile.open(data_files[0])
-            data_archive.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(data_archive)
         self.assertTrue(os.path.exists("screenshot-%s-80.jpg" % ipaddr))
         res, out, _ = RUN(["ivre", "scan2db", "--test"] + up_files)
         self.assertEqual(res, 0)
