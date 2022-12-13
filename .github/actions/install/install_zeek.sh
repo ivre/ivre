a1f766ab38e02bd3b4c6213e36cf06b698d83a45
Pierre Lalet <pierre@droids-corp.org>
2022-12-13 06:26:36 +0100

    lint: use shellcheck

--- .github/actions/install/install_zeek.sh
+++ .github/actions/install/install_zeek.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # This file is part of IVRE.
-# Copyright 2011 - 2021 Pierre LALET <pierre@droids-corp.org>
+# Copyright 2011 - 2022 Pierre LALET <pierre@droids-corp.org>
 #
 # IVRE is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
@@ -28,21 +28,23 @@ if [ "${ZEEK_VERSION:0:2}" = "4." ]; then
     cat tests/samples/results_zeek_v4 >> tests/samples/results
 fi
 
-UBUNTU_VERSION="`awk -F = '/^DISTRIB_RELEASE=/ {print $2}' /etc/lsb-release`"
+UBUNTU_VERSION="$(awk -F = '/^DISTRIB_RELEASE=/ {print $2}' /etc/lsb-release)"
 echo "UBUNTU_VERSION: ${UBUNTU_VERSION}"
 
-for archive in zeek-${ZEEK_VERSION}_ubuntu-${UBUNTU_VERSION}; do
-    wget -q --no-check-certificate https://ivre.rocks/data/tests/${archive}.tar.bz2 -O - | tar jxf -
+# shellcheck disable=SC2066
+for archive in "zeek-${ZEEK_VERSION}_ubuntu-${UBUNTU_VERSION}"; do
+    wget -q --no-check-certificate "https://ivre.rocks/data/tests/${archive}.tar.bz2" -O - | tar jxf -
 done
 
-for path_val in "`pwd`/usr/local/zeek/bin"; do
-    echo "$path_val" >> $GITHUB_PATH
+# shellcheck disable=SC2066
+for path_val in "$(pwd)/usr/local/zeek/bin"; do
+    echo "$path_val" >> "$GITHUB_PATH"
     export PATH="$path_val:$PATH"
 done
 
-for env_val in "ZEEKPATH=.:`pwd`/usr/local/zeek/share/zeek:`pwd`/usr/local/zeek/share/zeek/policy:`pwd`/usr/local/zeek/share/zeek/site" "ZEEKSAMPLES=`pwd`/usr/local/zeek/testing"; do
-    echo "$env_val" >> $GITHUB_ENV
-    export "$env_val"
+for env_val in "ZEEKPATH=.:$(pwd)/usr/local/zeek/share/zeek:$(pwd)/usr/local/zeek/share/zeek/policy:$(pwd)/usr/local/zeek/share/zeek/site" "ZEEKSAMPLES=$(pwd)/usr/local/zeek/testing"; do
+    echo "$env_val" >> "$GITHUB_ENV"
+    export "${env_val?}"
 done
 
-LD_LIBRARY_PATH=`pwd`/usr/local/zeek/lib zeek --version
+LD_LIBRARY_PATH="$(pwd)/usr/local/zeek/lib zeek --version"
