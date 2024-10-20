6725209b72d540c2e581ad935e8d0e4c06bba2ea
Pierre Lalet <pierre@droids-corp.org>
2024-10-20 22:54:12 +0200

    CI: 8.8.8.8 is no longer marked as Google CDN by cdncheck

--- tests/tests.py
+++ tests/tests.py
@@ -3739,7 +3739,6 @@ class IvreTests(unittest.TestCase):
     registered_country_name United States
     coordinates (37.751, -97.822)
     coordinates_accuracy_radius 1000
-    CDN: google as listed by cdncheck (projectdiscovery)
 """.splitlines()
             ),
         )
@@ -3760,13 +3759,6 @@ class IvreTests(unittest.TestCase):
                 "registered_country_name": "United States",
                 "coordinates": [37.751, -97.822],
                 "coordinates_accuracy_radius": 1000,
-                "tags": [
-                    {
-                        "value": "CDN",
-                        "type": "info",
-                        "info": ["google as listed by cdncheck (projectdiscovery)"],
-                    }
-                ],
             },
         )
 
