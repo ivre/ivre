e1ca2639f88614ad48337aa3c460b37493c707d6
Pierre Lalet <pierre@droids-corp.org>
2025-01-13 00:36:49 +0100

    CI: remove broken doc action

--- ivre/utils.py
+++ ivre/utils.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # This file is part of IVRE.
-# Copyright 2011 - 2024 Pierre LALET <pierre@droids-corp.org>
+# Copyright 2011 - 2025 Pierre LALET <pierre@droids-corp.org>
 #
 # IVRE is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
